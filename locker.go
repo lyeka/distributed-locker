5b170e75fc50e40f955116b3a9faf96282178876
yeka.lmh <yeka.lmh@mobvoyage.com>
2021-08-16 20:22:47 +0800

    add comment

--- locker.go
+++ locker.go
@@ -5,7 +5,15 @@ import (
 	"time"
 )
 
+// Locker 分布式锁实现接口
 type Locker interface {
-	Lock(ctx context.Context, ex time.Duration) (bool, error)
+	// Lock 加锁
+	// param
+	// ex: 锁过期时间
+	// return
+	// isSet: 是否成功加锁
+	Lock(ctx context.Context, ex time.Duration) (isSet bool, err error)
+
+	// UnLock 释放锁
 	UnLock(ctx context.Context) error
 }
